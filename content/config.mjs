9db4da3d243f18bce7f5ec60328a7b65381d856e
Alex Smekhnov <alexander.smehnov@gmail.com>
2024-09-04 15:38:16 +0300

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -3,7 +3,7 @@ const config = {
   title: "ICT Courses",
   description: "Content of ICT courses",
   author: "Alex Smekhnov",
-  logo: "/images/logo.svg",
+  logo: "/images/logo.png",
   //domain: "https://john.app/",
   // links to the pages you want to link to in the navbar and in the footer
   // navLinks: [{ href: "/about", name: "About" }],
